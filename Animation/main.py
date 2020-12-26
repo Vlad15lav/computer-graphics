7709cd99779a833781eb241f1f086208d9ca6de6
Vladislav Viryasov <62297940+Vlad15lav@users.noreply.github.com>
2020-12-26 23:08:11 +0300

    Add files via upload

---
+++ Animation/main.py
@@ -0,0 +1,19 @@
+import argparse
+import os
+from utils.animator import animator
+
+def get_args():
+    parser = argparse.ArgumentParser('Computer Graphics: Bresenham - Vlad15lav')
+    parser.add_argument('-f', '--ffmpeg', type=str, default='ffmpeg.exe', help='path ffmpeg file')
+    parser.add_argument('-p', '--path', type=str, default='source/teapot.obj', help='path obj file')
+    parser.add_argument('-b', '--bgcolor', type=int, nargs="+", default=[255, 255, 255], help='background color')
+    parser.add_argument('--frames', type=int, default=100, help='frames count')
+    parser.add_argument('--fps', type=int, default=24, help='fps amim')
+    parser.add_argument('--interval', type=int, default=30, help='interval amim')
+
+    args = parser.parse_args()
+    return args
+
+if __name__ == '__main__':
+    opt = get_args()
+    animator(opt.ffmpeg, opt.path, opt.bgcolor, opt.frames, opt.fps, opt.interval)
